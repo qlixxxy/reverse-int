c5f853942b15573296c3ca9c5c5fe29fca2e0a18
qlixxxy <sqlixor@gmail.com>
2020-03-22 20:13:03 +0300

    feat: complete project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
+    let str = n.toString()
+    let resultString = str.split('').reverse().filter(item => (item === "-") ? false : true).join('')
+    return resultString
+
+    
+
   
 }
